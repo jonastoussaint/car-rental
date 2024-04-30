97080decf3927e0cb82e2ce3a21da9c11dd8cc49
jonastoussaint <jonas.toussaint91@gmail.com>
2024-04-29 21:30:23 -0400

    car-rental-angular

---
+++ src/app/modules/customer/componets/customer-dashboard/customer-dashboard.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-customer-dashboard',
+  standalone: true,
+  imports: [],
+  templateUrl: './customer-dashboard.component.html',
+  styleUrl: './customer-dashboard.component.scss'
+})
+export class CustomerDashboardComponent {
+
+}
